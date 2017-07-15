d2574f425b886a635906195ba57590a803de57aa
David Adeola <adeoladavid15@gmail.com>
2017-07-15 17:04:08 +0300

    will add functionality to the update route of the application

--- app/components/update-tile.js
+++ app/components/update-tile.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions:{
+    editQuestion(message){
+      var params={
+        author: this.get('author'),
+        question: this.get('question'),
+        additionalinfo: this.get('additionalinfo')
+      };
+      this.sendAction('editQuestion',message,params);
+    }
+  }
 });
